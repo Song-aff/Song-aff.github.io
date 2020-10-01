8b3af68993a720a3cc885f4d9a4f20af35c99044
Song-aff <281218023@qq.com>
2020-10-01 20:56:30 +0800

    Updates

---
+++ precache-manifest.138b3cba1d8d5747ae8160c12e71c12c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293bab1aa03cd6e6f23d3a51563a2fcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c73db39454897a076c27",
+    "url": "/static/css/main.56a86cf6.chunk.css"
+  },
+  {
+    "revision": "e04b774b31b498fc00cd",
+    "url": "/static/js/2.35c1fc8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35c1fc8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c73db39454897a076c27",
+    "url": "/static/js/main.1000c32d.chunk.js"
+  },
+  {
+    "revision": "e4bf8d3e3687691e62de",
+    "url": "/static/js/runtime-main.691a3fc5.js"
+  }
+]);
